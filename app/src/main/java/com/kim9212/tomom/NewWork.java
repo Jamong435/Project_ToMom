b517d93cb29f96497141f2e3b8d654bd2e3b34e4
kim <sesang435@naver.com>
2020-09-10 22:41:45 +0900

    COMMIT

--- app/src/main/java/com/kim9212/tomom/NewWork.java
+++ app/src/main/java/com/kim9212/tomom/NewWork.java
@@ -2,13 +2,40 @@ package com.kim9212.tomom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.FragmentManager;
 import android.os.Bundle;
 
-public class NewWork extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class NewWork extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_work);
+
+        fragmentManager=getFragmentManager();
+        mapFragment=(MapFragment)fragmentManager.findFragmentById(R.id.googlemap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location= new LatLng(37.511548, 127.098722);
+        MarkerOptions markerOptions= new MarkerOptions();
+        markerOptions.title("가상사무실");
+        markerOptions.snippet("010-1234-5678");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location,16));
     }
 }
--- app/src/main/java/com/kim9212/tomom/YoutubeActivity.java
+++ app/src/main/java/com/kim9212/tomom/YoutubeActivity.java
@@ -3,8 +3,6 @@ package com.kim9212.tomom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -13,25 +11,24 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends AppCompatActivity {
 
-    YouTubePlayerView youTubePlayerView;
-   YouTubePlayerFragment youTubePlayerFragment;
+    YouTubePlayerView youTubeView;
+    YouTubePlayerFragment youTubeFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
 
-       youTubePlayerFragment= (YouTubePlayerFragment)getFragmentManager().findFragmentById(R.id.youtubeFragment);
-       youTubePlayerFragment.initialize("first", new YouTubePlayer.OnInitializedListener() {
-           @Override
-           public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-               youTubePlayer.loadVideo("hpSXpH5erqY");
-           }
+        youTubeFragment= (YouTubePlayerFragment)getFragmentManager().findFragmentById(R.id.youtubeFragment);
+        youTubeFragment.initialize("first", new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("hpSXpH5erqY");
+            }
 
-           @Override
-           public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
 
-           }
-       });
-
+            }
+        });
     }
 }
